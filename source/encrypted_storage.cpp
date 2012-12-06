7678fd4c4a3ef26448faf1a688ccbd761339025b
Tanner Smith <tanner@tssoftware.net>
2012-12-06 16:26:58 -0500

    EncryptedStorage overrides open/close.

--- source/encrypted_storage.cpp
+++ source/encrypted_storage.cpp
@@ -8,3 +8,11 @@ EncryptedStorage::EncryptedStorage(string path) : Storage(path) {
 
 }
 
+sqlite3* EncryptedStorage::open() {
+    return Storage::open();
+}
+
+int EncryptedStorage::close(sqlite3* db) {
+    return Storage::close(db);
+}
+
